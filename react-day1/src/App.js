ae214209b7af770d4ebfee0112f14bdd7f5f0d19
Subhanshu Chaurasiya <subhanshu123823@gmail.com>
2026-01-20 17:03:39 +0530

    Day 1: Spring Boot backend and React frontend setup

---
+++ react-day1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div>
+        <h1>React Day1</h1>
+        <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-day1/src/Hello.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Hello(){
+    return <h2>Hello from react Component</h2>
+}
+export default Hello;
